f6e5b15d613284903d9b97fec113867e27613efa
heynarmatha <hey.narmatha@gmail.com>
2022-05-08 13:54:22 +0530

    final

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
       <main>
         {/* <MultiTypography/> */}
         {/* <MuiButton/> */}
+        
         <MuiTextField/>
         <UiSelect/>
       </main>
